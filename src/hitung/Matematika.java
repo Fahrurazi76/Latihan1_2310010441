5ff40409da572b0a996e3e77dba632f9ac71f8f6
Fahrurazi76 <ahmadfahrurazi7@gmail.com>
2025-06-18 16:04:11 +0800

    Membuat Class MatematikaBeraksi

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author ASUS
- */
 public class Matematika {
+    //membuat variabel
+    private double bill, bil2;
     
+    //membuat constructor
+    public Matematika(double bil1, double bil2){
+        this.bill = bill;
+        this.bil2 = bil2;
+    }
+    
+    //membuat method setpenjumlahan
+    public double setpenjumlahan(){
+        return bill + bil2;
+    }
+    
+    //membuat method setpengurangan
+    public double setpengurangan(){
+        return bill - bil2;
+    }
+    
+    //membuat method setperkalian
+    public double setperkalian(){
+        return bill * bil2;
+    }
+    
+    //membuat method setpembagian
+    public double setpembagian(){
+        return bill / bil2;
+    }
 }
--- src/hitung/Matematikaberaksi.java
+++ src/hitung/Matematikaberaksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author ASUS
- */
 public class Matematikaberaksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika ozi = new Matematika(4,1);
+        
+        System.out.println("Hasil penjumlahan: "+ozi.setpenjumlahan());
+        System.out.println("Hasil pengurangan: "+ozi.setpengurangan());
+        System.out.println("Hasil perkalian: "+ozi.setperkalian());
+        System.out.println("Hasil pembagian: "+ozi.setpembagian());
+    }
+    }
+         
+            
 }
